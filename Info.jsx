bf97ace851f1ff2893d513e5c8af4c82eae0a772
husain-bori <84732144+husain-bori@users.noreply.github.com>
2021-06-26 20:50:15 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript And React.js</h1>
+      <p>
+      This was an Amazing Bootcamp.
+      It was a basic web dev Reactjs Bootcamp.
+      Our instructor was Shaurya Sinha.
+      We learned a little of Html and CSS.
+      Also, we learned majorly Javascript and Reactjs.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
